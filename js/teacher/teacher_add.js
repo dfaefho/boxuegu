d931dad89e4b2c23e41e98f334c3354642f447d1
xuxin <15279969124@139.com>
2017-04-08 21:51:11 +0800

    updata new

--- js/teacher/teacher_add.js
+++ js/teacher/teacher_add.js
@@ -1,3 +1,16 @@
-define(['aside'], function() {
+define(['aside', 'header', 'loading', 'nprogress', 'jquery'], function(undefined, undefined, undefined, nprogress, $) {
 	
+	// 添加讲师，成功后跳转到讲师列表页
+	(function() {
+		
+		$('#teacher-add-edit-form').on('submit', function() {
+			$.post('/v6/teacher/add', $(this).serialize(), function(data) {
+				location.href = '/html/teacher/teacher_list.html';
+			});
+			return false;
+		});
+		
+	})();
+	
+	nprogress.done();
 });
